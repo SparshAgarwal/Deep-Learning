f65b5e8f6c6b29e9ee4d5cdc9330a0a31a5e47c2
Sparsh <agarwal39@wisc.edu>
2017-02-01 02:20:11 -0600

    first files

--- src/Lab1.java
+++ src/Lab1.java
@@ -5,6 +5,6 @@ public class Lab1 {
     public static void main(String args[]){
         scanner train = new scanner();
         train.read_files(args[0]);
-        System.out.print(train.examples.get(2));
+        
     }
 }
